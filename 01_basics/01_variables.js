8084313d12e9336b42a17d543246a6b4a2329e42
anchitapanjeta <106548839+anchitapanjeta@users.noreply.github.com>
2025-01-02 14:05:56 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountEmail = "Anchitapanjeta@gmail.com"
+var accountPassword = "12345"
+accountCity = "Yamunanagar"
+
+// accountId = 2 not allowed
+
+accountEmail = "anchi@gmail.com"
+accountPassword = "2213131"
+accountCity = "Banglore"
+
+console.log(accountId);
+
