4d5b19229ef92e534a13a3ad0a5654aeed15bc02
anchitapanjeta <106548839+anchitapanjeta@users.noreply.github.com>
2025-01-03 17:28:11 +0000

    Commit

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score = "33"
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NAN  NAN ka type number
+//true =>1 ; false => 0;
+
+// "" => false
+//"anchita" =>true
+
+let loggedin = 1;
+let booleanisLoggedIN = Boolean(loggedin);
+console.log(booleanisLoggedIN);
+
+let someNo = 33;
+let stringNum = String(someNo)
+console.log(typeof stringNum);
