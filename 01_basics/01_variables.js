cf35edbe4fd7798fef912584f30e0e93f9b3bc29
anchitapanjeta <106548839+anchitapanjeta@users.noreply.github.com>
2025-01-03 17:07:06 +0000

    Commit

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,13 +4,13 @@ var accountPassword = "12345"
 accountCity = "Yamunanagar"
 
 // accountId = 2 not allowed
-
+let accountState;
 accountEmail = "anchi@gmail.com"
 accountPassword = "2213131"
 accountCity = "Banglore"
 
 console.log(accountId);
 
-console.log("Anchita")
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 
 
